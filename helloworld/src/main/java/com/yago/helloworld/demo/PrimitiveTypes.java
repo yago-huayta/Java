56174a266efe18c9a5cf3356c21c1d77c580e331
Yago Huayta <yago.huayta@outlook.com>
2020-09-30 00:28:38 -0500

    Cambie el segundo texto a ingles

--- helloworld/src/main/java/com/yago/helloworld/demo/PrimitiveTypes.java
+++ helloworld/src/main/java/com/yago/helloworld/demo/PrimitiveTypes.java
@@ -3,5 +3,6 @@ package com.yago.helloworld.demo;
 public class PrimitiveTypes {
     public static void main(String... args){
         System.out.println("Hello, World!");
+        System.out.println("second text in Java");
     }
 }
