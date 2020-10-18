7ed930a5fb053f3c767f68e9325c08b3886cd85b
Yago Huayta <yago.huayta@outlook.com>
2020-10-17 21:54:23 -0500

    Ejercicios con java

--- helloworld/src/main/java/com/yago/helloworld/demo/PruebasMain.java
+++ helloworld/src/main/java/com/yago/helloworld/demo/PruebasMain.java
@@ -1,18 +1,111 @@
 package com.yago.helloworld.demo;
 
 public class PruebasMain {
-    public static  void main(String... arg){
-        People juan = new People();
+    public static void main(String... arg) {
+        /* People juan = new People();
 
         juan.surname = "Garcia";
         juan.work = "Engineer";
 
-        System.out.println("el apellido de juan es: "+juan.surname);
+        System.out.println("el apellido de juan es: " + juan.surname);
 
-        System.out.println("La energia incial de juan es: "+ juan.getEnergy());
+        System.out.println("La energia incial de juan es: " + juan.getEnergy());
 
         juan.run(30);
 
-        System.out.println("Juan salio a corrar y ahora tiene "+ juan.getEnergy()+" de energia");
+        System.out.println("Juan salio a corrar y ahora tiene " + juan.getEnergy() + " de energia");
+
+        int helloWorld = 0;
+
+        while (helloWorld <= 100) {
+            helloWorld++;
+            System.out.println("helloWorld");
+        }
+         */
+        bucleSumativo(5);
+        sumando(3);
+        sumarPares(5, false);
+
     }
+
+    private static void bucleSumativo(int n) {
+
+        int contenedor = 0;
+
+        for (int box = 0; box <= n; box++) {
+
+            contenedor = box + contenedor;
+
+        }
+
+        System.out.println("La suma es: " + contenedor);
+
+    }
+
+
+    private static void sumando(int x) {
+
+        int contenedor = 0;
+        int box = 0;
+
+        while (box <= x) {
+
+            box++;
+
+            contenedor = box + contenedor;
+
+        }
+        if (contenedor < 20) {
+            System.out.println("La sumatoria es menor que 20");
+        } else {
+            System.out.println("La sumatoria es mayor que 20");
+        }
+    }
+
+    public static void sumarPares(int y, boolean par) {
+
+        int contenedor = 0;
+
+        for (int box = 0; box <= y; box++) {
+
+            if (par == true) {
+                if (box % 2 == 0) {
+                    contenedor = contenedor + box;
+                }
+
+            }else{
+                if (box % 2 == 1) {
+                    contenedor = contenedor + box;
+                }
+
+            }
+
+        }
+        System.out.println(contenedor);
+
+        /*if (par = true) {
+            for (int box = 0; box <= y; box++) {
+                if (box % 2 == 0) {
+                    contenedor = contenedor + box;
+                }
+            }
+            System.out.println(contenedor);
+        }else{
+            for (int box = 0; box <= y; box++) {
+                if (box % 2 == 1) {
+                    contenedor = contenedor + box;
+                }
+            }
+            System.out.println(contenedor);
+        }
+
+         */
+    }
+
 }
+
+
+
+
+
+
